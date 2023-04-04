52ec4edd8811d63ed669ad9158e35660028acd6c
Rainier-MSFT <rainieruk@live.co.uk>
2023-04-04 15:50:51 +0100

    Update

--- Base-config_3-vm/scripts/Common_Configs.ps1
+++ Base-config_3-vm/scripts/Common_Configs.ps1
@@ -1,5 +1,5 @@
 <#  Common_Configs.ps1
-    Rainier Amara 9/1/2023
+    Rainier Amara 9/1/23
     This script applies a bunch of pre-configurations to each VM, post ARM deployment.
 #>
 
--- Base-config_3-vm/scripts/Deploy-TL.ps1
+++ Base-config_3-vm/scripts/Deploy-TL.ps1
@@ -1,7 +1,7 @@
-﻿<#  Deploy-TLG.ps1
-    Kelley Vice 7/11/2018
+﻿<#  Deploy-TL.ps1
+    Rainier Amara 9/1/23
 
-    This script deploys the TLG (Test Lab Guide) 3 VM Base Configuration to your Azure RM subscription.
+    This script deploys the 3 VM Base configuration lab to your Azure RM subscription.
     You must have the AzureRM PowerShell module installed on your computer to run this script.
     To install the AzureRM module, execute the following command from an elevated PowerShell prompt:
 
@@ -12,20 +12,21 @@
 # Provide parameter values
 $subscription = "subscription name"
 $resourceGroup = "resource group name"
-$location = "location, i.e. West US"
+$location = "location, i.e. UK South"
 
-$configName = "" # The name of the deployment, i.e. BaseConfig01. Do not use spaces or special characters other than _ or -. Used to concatenate resource names for the deployment.
-$domainName = "" # The FQDN of the new AD domain.
-$serverOS = "2016-Datacenter" # The OS of application servers in your deployment, i.e. 2016-Datacenter or 2012-R2-Datacenter.
-$adminUserName = "" # The name of the domain administrator account to create, i.e. globaladmin.
-$adminPassword = "" # The administrator account password.
+$configName = "" # The name of the deployment, i.e. BaseConfig01. Do not use spaces or special characters other than _ or -. Used to concatenate resource names for the deployment
+$domainName = "" # The FQDN of the new AD domain
+$serverOS = "2016-Datacenter" # The OS of application servers in your deployment, i.e. 2016-Datacenter or 2012-R2-Datacenter
+$adminUserName = "" # The name of the domain administrator account to create, i.e. globaladmin
+$adminPassword = "" # The administrator account password
 $deployClientVm = "Yes" # Yes or No
-$clientVhdUri = "" # The URI of the storage account containing the client VHD. Leave blank if you are not deploying a client VM.
-$vmSize = "Standard_DS2_v2" # Select a VM size for all server VMs in your deployment.
-$dnsLabelPrefix = "" # DNS label prefix for public IPs. Must be lowercase and match the regular expression: ^[a-z][a-z0-9-]{1,61}[a-z0-9]$.
-$_artifactsLocation = "https://raw.githubusercontent.com/oualabadmins/lab_deploy/master/tlg-base-config_3-vm" # Location of template artifacts.
-$_artifactsLocationSasToken = "" # Enter SAS token here if needed.
+$clientVhdUri = "" # The URI of the storage account containing the client VHD. Leave blank if you are not deploying a client VM
+$vmSize = "Standard_B2s" # Select a VM size for all server VMs in your deployment
+$dnsLabelPrefix = "" # DNS label prefix for public IPs. Must be lowercase and match the regular expression: ^[a-z][a-z0-9-]{1,61}[a-z0-9]$
+$_artifactsLocation = "https://raw.githubusercontent.com/Rainier-MSFT/Entra_ZTNA_Lab/main/Base-config_3-vm/" # Location of template artifacts
+$_artifactsLocationSasToken = "" # Enter SAS token here if needed
 $templateUri = "$_artifactsLocation/azuredeploy.json"
+$enableBastion = "Yes" # Deploy a Bastion for VM management via a browser based RDP connection instead of exposing RDP directly to the WWW
 
 # Add parameters to array
 $parameters = @{}
