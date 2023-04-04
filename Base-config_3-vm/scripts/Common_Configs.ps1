518b0388a67774aff409eab80ec312ff2a202bea
Rainier-MSFT <rainieruk@live.co.uk>
2023-04-04 12:33:34 +0100

    Update

--- Base-config_3-vm/scripts/Common_Configs.ps1
+++ Base-config_3-vm/scripts/Common_Configs.ps1
@@ -1,9 +1,3 @@
-configuration CommonConfig
-{
-	
-# Import DSC modules
-    Import-DscResource -ModuleName PSDesiredStateConfiguration, xActiveDirectory, xDisk, xNetworking, cDis
-
 ## Enable TLS1.2
 New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols" -Name "TLS 1.2"
 New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2" -Name "Client"
@@ -51,4 +45,3 @@ $shortcut.Save()
 ## Download Azure AD Connect
 Import-Module BitsTransfer
 Start-BitsTransfer -Source "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi" -Destination "$env:PUBLIC\Desktop\Install Azure AD Connect.msi"
-}
